dc7560de41290cbec4021248d3eaa2939d490cf0
bryan <bescalante@institutmvm.cat>
2021-12-02 13:14:50 +0100

    Printar el nombre, y los apellidos las 2 primeras letras

--- Nombre.py
+++ Nombre.py
@@ -1,2 +1,10 @@
-nombre = str(input("Ingresa tu nombre"))
-print("Tu nombre es:", nombre)
+def main():
+
+
+    nombre = str(input("Ingresa tu nombre:"))
+    apellidos = str(input("Ingresa primer apellido:"))
+    apellido = str(input("Ingresa segundo apelledio:"))
+    print("Tu nombre es:", nombre, "El el apellido es: ", apellidos[:2], "El apellido:", apellido[:2])
+
+if __name__ == '__main__':
+    main()
