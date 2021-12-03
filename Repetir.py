f36c3beb51b5421baba4a1449ce67cc6e4569329
bryan <bescalante@institutmvm.cat>
2021-12-03 12:30:53 +0100

    Tabla de valores

---
+++ Repetir.py
@@ -0,0 +1,27 @@
+def main():
+
+
+    id = []
+    nombres = []
+    apellido = []
+    segundo_ape = []
+    edad = []
+
+    while x <= 0:
+        x = int(print("Introduze un nombre"))
+        id.append([x])
+
+        x = str(print("Introduze un nombre"))
+        nombres.append([x])
+
+        x = str(print("Introduze un nombre"))
+        apellido.append([x])
+
+        x = str(print("Introduze un nombre"))
+        segundo_ape.append([x])
+
+        x = int(print("Introduze un nombre"))
+        edad.append([x])
+
+if __name__ == '__main__':
+    main()
