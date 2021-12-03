53e86210907dd55d1ec5a87ca717f1e44b29054f
bryan <bescalante@institutmvm.cat>
2021-12-03 10:17:04 +0100

    printo todas las variables juntas

--- Nombre.py
+++ Nombre.py
@@ -4,7 +4,7 @@ def main():
     nombre = str(input("Ingresa tu nombre:"))
     apellidos = str(input("Ingresa primer apellido:"))
     apellido = str(input("Ingresa segundo apelledio:"))
-    print("Tu nombre es:", nombre, "El el apellido es: ", apellidos[:2], "El apellido:", apellido[:2])
+    print("Tu nombre es:", nombre + apellidos[:2] + apellido[:2])
 
 if __name__ == '__main__':
     main()
